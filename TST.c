19dbb2937f760ecd63f243b924b7cb35f9710a5b
EllyasHiagamo <ellyasantox43@gmail.com>
2026-06-25 12:39:59 -0300

    modificacoes em TST.h e TST.c (a continuar)

--- TST.c
+++ TST.c
@@ -12,26 +12,34 @@ struct tst
     DocList *docs;
 };
 
-TST *create_node()
+static TST *create_node()
 {
     TST *nova = (TST *)calloc(1, sizeof(TST));
 
     nova->esq = NULL;
     nova->meio = NULL;
     nova->dir = NULL;
-    nova->docs = cria_doclist();
-    nova->final = false;
+    nova->docs = NULL;
+    // nova->final = false; nao eh necessario, pode-se verificar se o no eh um "fim" pelo docklist
 
     return nova;
 }
-// key = "ana\0"
-// key = key + 1 = "na\0"
-// key = key + 1 = "a\0"
-// key = "\0"
+// key = "ana\0"           (na primeira chamada)
+// key' = key + 1 = "na\0" (na segunda chamada)
+// key'' = key + 1 = "a\0" (na terceira chamada)
+// key''' = "\0"           (na ultima chamada)
 
-TST *rec_insert(TST *t, char *key)
+TST *cria_tst()
 {
-    unsigned char c = *key; // key[d]
+    return NULL;
+}
+
+// MODIFICAR PARA O CASO DE TENTAR INSERIR UMA PALAVRA QUE JA ESTA NA TST
+// NESTE CASO, MODIFICAR A DOCKLIST DESTA ARVORE, VALUE EH O DOCUMENTO ATUAL DE ORIGEM DESSA PALAVRA
+static TST *rec_insert(TST *t, char *key)
+{
+    unsigned char c = *key; // se key = "ana\0" -> *key = 'a' (primeiro caracter)
+
     if (t == NULL)
     {
         t = create_node();
@@ -48,10 +56,12 @@ TST *rec_insert(TST *t, char *key)
         t->dir = rec_insert(t->dir, key);
     }
 
-    else if (*(key + 1) == '\0')
-        t->final = true;
-    else
+    else if (*(key + 1) != '\0')
+    {
         t->meio = rec_insert(t->meio, key + 1);
+    }
+    else
+        t->docs = cria_doclist();
 
     return t;
 }
@@ -59,4 +69,64 @@ TST *rec_insert(TST *t, char *key)
 TST *TST_insert(TST *t, char *key)
 {
     return rec_insert(t, key);
+}
+
+static TST *rec_search(TST *t, char *key)
+{
+    if (t == NULL)
+    {
+        return NULL;
+    }
+
+    unsigned char c = *key;
+
+    if (c < t->c)
+    {
+        return rec_search(t->esq, key);
+    }
+    else if (c > t->c)
+    {
+        return rec_search(t->dir, key);
+    }
+    else if (*(key + 1) != '\0')
+    {
+        return rec_search(t->meio, key + 1);
+    }
+    else
+    {
+        return t;
+    }
+}
+
+DocList *TST_search(TST *t, char *key)
+{
+    t = rec_search(t, key);
+
+    if (t == NULL)
+    {
+        return NULL;
+    }
+
+    return t->docs;
+}
+
+static TST *rec_desloca(TST *t)
+{
+    if (t == NULL)
+    {
+        return NULL;
+    }
+
+    t->esq = rec_desloca(t->esq);
+    t->dir = rec_desloca(t->dir);
+    t->meio = rec_desloca(t->meio);
+
+    free_doclist(t->docs);
+
+    return NULL;
+}
+
+TST *desaloca_tst(TST *t)
+{
+    return rec_desloca(t);
 }
--- TST.h
+++ TST.h
@@ -4,8 +4,34 @@
 #include "lista_docs.h"
 #include <stdbool.h>
 
-// OBS: passar structs para o .c depois para a estrutura ficar opaca.
-
 typedef struct tst TST;
+
+/**
+ * @brief inicializa uma tst nula
+ * @return NULL
+ */
+TST *cria_tst();
+
+/**
+ * @brief insere um chave (string/palavra) na tst
+ * @param t trie a ser modificada
+ * @param key string a ser adcionada na arvore
+ * @return tst atualizada
+ */
+TST *TST_insert(TST *t, char *key);
+
+/**
+ * @brief busca uma string/palavra em uma tst
+ * @param t arvore a ser concatenada
+ * @param key palavra a ser buscada
+ * @return se for encontrada: a lista de documentos onde esta palavra aparece; se não for encontrada: NULL
+ */
+DocList *TST_search(TST *t, char *key);
+
+/**
+ * @brief desaloca uma tst
+ * @return tst atualizada (NULL)
+ */
+TST *desaloca_tst(TST *t);
 
 #endif
